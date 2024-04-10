d40217e98d7e6d1e43524b33261e04a9f43a8835
Iago Veiga <iago@veiga.top>
2024-04-10 11:40:50 +0200

    Backup npc folder

---
+++ backup/npc/boot.sh
@@ -0,0 +1,8 @@
+#start npc boot
+
+cp /npc/sensors/* /etc/sensors/ -Rf
+
+if [ -e /patch/lib/mt7601Usta_v2.ko ] ; then
+insmod /patch/lib/mt7601Usta_v2.ko
+fi
+sync;sync;sync && echo 3 > /proc/sys/vm/drop_caches
